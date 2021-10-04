04334cfecbfb332ba3583f0ffc27b5b12e23f3bf
rnimax <robert.nimax@inform-software.com>
2021-10-04 08:07:35 +0200

    121

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,27 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe(
+        'Schnitzel', 
+        'Tasty Schnitzel', 
+        'https://www.gutekueche.at/storage/media/recipe/8028/resp/wiener-schnitzel___webp_460_306.webp',
+        [
+          new Ingredient('Meat' , 2),
+          new Ingredient('French fries', 60)
+        ]),
+    new Recipe(
+        'Burger', 
+        'Tasty Burger', 
+        'https://www.gutekueche.at/storage/media/recipe-category/7755/resp/burger-rezepte___webp_849_566.webp',
+        [
+          new Ingredient('Buns' , 2),
+          new Ingredient('Paddies', 2)
+        ]),
   ];
 
   getRecipes() : Recipe[] {
